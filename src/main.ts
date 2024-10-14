e21b36e238a2c4acbb892aae4b7e464a0a995e52
Cheristic <ethanheffan@gmail.com>
2024-10-13 17:51:18 -0700

    Step 2 drawing and clearing

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 
 
-
-function SetupHeader() {
+function SetupPage() {
     const header = document.createElement("div");
     header.setAttribute(
     "style",
@@ -17,13 +16,17 @@ function SetupHeader() {
     headerText.innerHTML = "Welcome to KidPix";
     headerText.setAttribute("style", "margin-top: 0px; padding-top: 20px;");
     header.append(headerText);
+
+    
+    
 }
 
-let canvas: HTMLCanvasElement | null;
-let ctx: CanvasRenderingContext2D | null;
+let canvas;
+let ctx;
+let cursor = {active: false, x: 0, y:0}
 function main() {
-    SetupHeader();
-    
+    SetupPage();
+
     canvas = document.querySelector<HTMLCanvasElement>('canvas');
     if (!canvas) {
         console.log('Failed to retrieve the <canvas> element');
@@ -36,9 +39,38 @@ function main() {
         return;
     }
 
-    console.log(canvas);
     ctx.fillStyle = '#FFFFFF'
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#000000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvas.addEventListener('mousedown', (e) => {
+        cursor.active = true;
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    })
+
+    canvas.addEventListener('mousemove', (e) => {
+        if (cursor.active) {
+            ctx?.beginPath();
+            ctx?.moveTo(cursor.x, cursor.y);
+            ctx?.lineTo(e.offsetX, e.offsetY);
+            ctx?.stroke();
+
+            cursor.x = e.offsetX;
+            cursor.y = e.offsetY;
+        }
+    })
+
+    canvas.addEventListener('mouseup', () => {
+        cursor.active = false;
+    })
+
+    const clearButton = app.appendChild(document.createElement('button'));
+    clearButton.innerHTML = "clear";
+    clearButton.addEventListener('click', () => {
+        ctx?.fillRect(0, 0, canvas!.width, canvas!.height);
+    })
 }
 
 main();
